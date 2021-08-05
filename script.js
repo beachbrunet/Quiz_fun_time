e7b52308118a9f63823a94586b0bf96bb7befa40
Catherine Cox <ccox.034@gmail.com>
2021-08-05 15:11:34 -0600

    Cleaned up Pseudo code, edited JS and HTMl to reflect that

--- script.js
+++ script.js
@@ -5,17 +5,21 @@
 // Second: After selecting the answer it will move onto the next question.
 // Last: Quiz ends when all the questions have been answered or the timmer reaches zero.
 
-// Basic outline for questions - addEventListener
-// When user selects an answer
-// Check if it was correct
-// Inform them if they got it right or wrong
+// MVP (Minimum Viable Product)
+// // Shows a user a question with possible answers
+// Choices are clickable
+// When choice is clicked app determines if it is correct or not
+// Show next question and let them do it again
+// Showing the user it's score at end
 
 
-// function handleFormSubmit(event) {
-// event.preventDefault();
-//  }
+// FEATURES (bonus which just makes it better)
+// start button
+// When hitting the start timer starts
+// deducting time when someone answers wrong
+// showing other peoples high scores
+// storing high scores
 
- var score = 0;
 
 // Questions
 
@@ -23,35 +27,48 @@ var trivaQuestions = [
     {
         question: "Which one of these is a Charectors you can play in Overwatch?",
         possibles: ["Reaper", "Deathstroke", "Axion", "Sergeant-Barrister Jane Doe"],
-        answer: 0,
+        answer: "Reaper",
     },
     {
         question: "In Bungie's Destiny game series raid appeared in both Destiny and Destiny 2?",
         possibles: ["Wrath of the Machine", "Last Wish", "Vault of Glass", "Kings Fall"],
-        answer: 2,
+        answer: "Vault of Glass",
     },
     {
         question: "In what Series where Dracula and The Grim Reaper recurring Villians",
         possibles: ["Halo", "Dark Souls", "Soul Reaver", "Castlevania"],
-        answer: 3,
+        answer: "Castlevania",
     },
     {
         question: "Which one of these is not a Rockstar developed game?",
         possibles: ["Grand Theft Auto (GTA)", "Assassin's Creed", "Bully", "Red Dead Redemption 2"],
-        answer: 2,
+        answer: "Assassin's Creed",
     },
     {
         question: "Kratos is the main Charecter of which game?",
         possibles: ["God Hand", "Assassin's Creed", "God of War", "Prince of Persia"],
-        answer: 0, 
+        answer: "God of War",
     },
     {
         question: "Did you enjoy this Quiz?",
         possibles: ["Yes", "No"],
-        answer: 0,
+        answer: "Yes",
     },
 
 ];
+
+// TO show questions and answers
+var indexOfQuestionToShow = 1
+
+document.querySelector('#question').textContent = trivaQuestions[indexOfQuestionToShow].question
+
+for (let i = 0; i < trivaQuestions[indexOfQuestionToShow].choices.length; i++) {
+	var choiceButton = document.createElement('button')
+	choiceButton.textContent = trivaQuestions[indexOfQuestionToShow].choices[i]
+	// add a class so that you can make event listener later
+	document.querySelector('#choices').appendChild(choiceButton)
+}
+
 
 // Function opperations. 
 // -Show me Question
@@ -67,18 +84,13 @@ var trivaQuestions = [
 
 function startGame () {
     startButton.destroy();
-
-// Run game
-document.getElementById('Start-Button')
-startButton.addEventListener("click", startGame)
-
-
     // addEventListener("click", "Start-Button").style.display = "hidden";
 
+// Run game
+document.getElementById('Start-Button');
+startButton.addEventListener("click", startGame)
 
 }
-
-
 
 
 
